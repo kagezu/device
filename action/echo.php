c3c9f5a6ad91d9155dab97f644c40aa2312d3b25
hinto <hinto@rambler.ru>
2022-09-27 00:35:12 +0700

    Вспомогательные компоненты

---
+++ action/echo.php
@@ -0,0 +1,8 @@
+<?php
+echo "POST <br><br>";
+foreach($_POST as $key => $value)
+	echo "[" . $key . "] => " . $value . "<br>";
+
+echo "<br><hr>";
+?>
+<h2><a href="../index.html">Вернуться</a></h2>
